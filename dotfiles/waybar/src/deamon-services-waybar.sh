bcdc9a3013137dc6755d2489361b0e4dd31ecdda
dev-rajnish <dev.rajnish@proton.me>
2025-07-09 19:05:24 +0530

    added dotfiles/waybar

--- dotfiles/waybar/src/deamon-services-waybar.sh
+++ dotfiles/waybar/src/deamon-services-waybar.sh
@@ -2,28 +2,28 @@
 # 5. Tee to hyprland.conf if not already included
 HYPR_CONF="$HOME/.config/hypr/hyprland.d/deamons-services-autostart.conf"
 
-if ! grep -q "sww-deamon" "$HOME/.config/hypr/hyprland.d/deamons-services-autostart"; then
+if ! grep -q "sww-deamon"  "$HYPR_CONF"; then
     echo "exec-once = swww-daemon &" >> "$HYPR_CONF"
 
 else
     echo "→ Already included in hyprland.conf"
 fi
 
-if ! grep -q "swww restore" "$HOME/.config/hypr/hyprland.d/deamons-services-autostart"; then
+if ! grep -q "swww restore" "$HYPR_CONF"; then
     echo "exec-once = swww restore &" >> "$HYPR_CONF"
 
 else
     echo "→ Already included in hyprland.conf"
 fi
 
-if ! grep -q "swaync" "$HOME/.config/hypr/hyprland.d/deamons-services-autostart"; then
+if ! grep -q "swaync" "$HYPR_CONF"; then
     echo "exec-once = swaync" >> "$HYPR_CONF"
 
 else
     echo "→ Already included in hyprland.conf"
 fi
 
-if ! grep -q "wl-paste" "$HOME/.config/hypr/hyprland.d/deamons-services-autostart"; then
+if ! grep -q "wl-paste" "$HYPR_CONF"; then
     echo "exec-once = wl-paste --type text --watch cliphist store &" >> "$HYPR_CONF"
     echo "exec-once = wl-paste --type image --watch cliphist store &" >> "$HYPR_CONF"
   
@@ -31,7 +31,7 @@ if ! grep -q "wl-paste" "$HOME/.config/hypr/hyprland.d/deamons-services-autostar
     echo "→ Already included in hyprland.conf"
 fi
 
-if ! grep -q "hypridle" "$HOME/.config/hypr/hyprland.d/deamons-services-autostart"; then
+if ! grep -q "hypridle" "$HYPR_CONF"; then
     echo "exec-once = hypridle &" >> "$HYPR_CONF"
 
 else
@@ -39,7 +39,7 @@ else
 fi
 
 
-if ! grep -q "waybar" "$HOME/.config/hypr/hyprland.d/deamons-services-autostart"; then
+if ! grep -q "waybar" "$HYPR_CONF"; then
     echo "exec-once = waybar &" >> "$HYPR_CONF"
 
 else
